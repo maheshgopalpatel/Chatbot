e8b4179ec81a0d996ed3c0bce7e4e7e986a8ea3d
Mahesh Patel <maheshgopalpatel@gmail.com>
2024-02-12 18:49:36 +0530

    add result section

--- src/chatbot/Chatbot.jsx
+++ src/chatbot/Chatbot.jsx
@@ -4,10 +4,10 @@ import React, { useEffect, useState } from "react";
 const hiringQuestions = [
   "What position are you hiring for?",
   "What Specific skill are required for this position?",
-  "How many years of experience is needed?",
-  "What is the minimum education requirement?",
-  "What location is this position you want?",
-  "Is this position remote?",
+  // "How many years of experience is needed?",
+  // "What is the minimum education requirement?",
+  // "What location is this position you want?",
+  // "Is this position remote?",
 ];
 
 function Chatbot() {
@@ -28,17 +28,28 @@ function Chatbot() {
   const [que, setQue] = useState(0);
   console.log(final);
   const handleSendMessage = () => {
-    if (inputValue.trim() !== "" && hiringQuestions.length >= que + 1) {
+    if (inputValue.trim() !== "" && hiringQuestions.length > que + 1) {
       setMessages([...messages, { text: inputValue, sender: "user" }]);
       setFinal({ ...final, [arr[que]]: inputValue });
       setInputValue("");
       setQue(que + 1);
     } else {
+      setFinal({ ...final, [arr[que]]: inputValue });
       setMessages([...messages, { text: obj.Assistant, sender: "bot" }]);
-      setInputValue("");
+      setQue(que + 1);
     }
   };
-  console.log(que, hiringQuestions.length);
+  // console.log(que, hiringQuestions.length);
+  const resultHandler = (value) => {
+    if (value == "cancel") {
+      console.log("@@@@@@@@@@@@");
+      setInputValue("");
+      setMessages([])
+      setQue(0)
+    } else {
+      setInputValue("completed");
+    }
+  };
   const handleQuestion = () => {
     if (inputValue.trim() == "") {
       setMessages([...messages, { text: hiringQuestions[que], sender: "bot" }]);
@@ -51,24 +62,44 @@ function Chatbot() {
   return (
     <div className="chatbot-container">
       <div className="chatbot-messages">
-        {que !== hiringQuestions.length
-          ? messages.map((message, index) => {
+        {inputValue == "completed" ? (
+          <div className="result">
+            {Object.values(final).map((item, i) => {
+              let arr = Object.keys(final);
               return (
-                <div
-                  key={index}
-                  className={`message ${
-                    message.sender === "user" ? "user-message" : "bot-message"
-                  }`}
-                >
-                  {message.text}
-                </div>
+                <>
+                  <div>{arr[i]}</div>
+                  <div>{item}</div>
+                </>
               );
-            })
-          : ""}
+            })}
+          </div>
+        ) : (
+          messages.map((message, index) => {
+            return (
+              <div
+                key={index}
+                className={`message ${
+                  message.sender === "user" ? "user-message" : "bot-message"
+                }`}
+              >
+                {message.text}
+              </div>
+            );
+          })
+        )}
       </div>
       <div>
         {hiringQuestions.length == que ? (
-          <div>here success massege and result</div>
+          <div className="chatbot-input">
+            <button
+              onClick={(e) => {
+                resultHandler(e.target.innerText);
+              }}
+            >
+              {inputValue == "completed" ? "cancel" : "submit"}
+            </button>
+          </div>
         ) : (
           <div className="chatbot-input">
             <input
